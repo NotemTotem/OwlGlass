e91b00cf90ce31c3d4a8f167b6c3acdebc54aca6
Nyonlexi <68531919+Nyonlexi@users.noreply.github.com>
2025-09-03 14:26:14 +1000

    Created accountfinder.py

---
+++ Toolscripts/accountfinder.py
@@ -0,0 +1,13 @@
+import argparse
+import requests
+
+parser = argparse.ArgumentParser()
+#-u username -e email
+parser.add_argument('-u',help='username')
+parser.add_argument('-e',help='email')
+
+
+def main():
+    pass
+
+main()
