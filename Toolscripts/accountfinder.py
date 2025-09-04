d8e9cfc3b7abd96bd4178f907ee339c8947b6957
Nyonlexi <68531919+Nyonlexi@users.noreply.github.com>
2025-09-04 13:18:21 +1000

    Improved error handling, Continued work on website checking and web-object/target-objects created.

--- Toolscripts/accountfinder.py
+++ Toolscripts/accountfinder.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import requests
 
 #In future websites will be stored in data.json 
 class WebsiteManager:
@@ -18,13 +19,15 @@ class WebsiteManager:
             'username_claimed':str
         }
         """
-        self.website_objects = {
+        self.web_objects = {
             "chess.com":{
                 'errorType':'status_code',
-                'url':'chess.com/member/{}',
+                'url':'chess.com/member/<target>',
                 'urlMain':'chess.com',
-                'username_claimed':'red'
+                'username_claimed':'red',
+                
             }
+    
         }
 
 class MyParser(argparse.ArgumentParser):
@@ -32,31 +35,75 @@ class MyParser(argparse.ArgumentParser):
         sys.stderr.write('error: %s\n' % message)
         # self.print_help()
         sys.exit(2)
-    
-websitemanager = WebsiteManager()
+    def warning(self,message):
+        sys.stderr.write('warning: %s\n' % message)
 parser = MyParser()
+websitemanager = WebsiteManager()
 
-parser.add_argument('-u','--username',help='username',nargs=1)
-parser.add_argument('-e','--email',help='email',nargs=1)
-parser.add_argument('-wl','--websitelist',help='lists scrapeable websites',nargs=0)
+
+def web_check(target_object,web_object):
+    if target_object["targetType"] == 'email':
+        target = target_object["targetSplit"][1]
+    elif target_object["targetType"] == 'username':
+        target = target_object["target"]
+    else:
+        parser.error("targetType of {s!r}" % f'{target_object["targetType"]} not present in web_check.')
+
+    url = web_object['url'].replace('<target>',target)
+
+    if web_object['errorType'] == 'status_code':
+        r = requests.get(url)
+        if r.status_code == 200:
+            return True
+        else:
+            return False
+    else:
+        parser.error("status_code of {s!r}" % f'{web_object["status_code"]} not currently implemented.')
+def main():
+
+    if args.e:
+        target_object = {
+            'target':args.e,
+            'targetSplit':args.e.split('@'),
+            'targetType':'email'
+        }
+    elif args.u:
+        target_object = {
+            'target':args.u,
+            'target_type':'username'
+        }
+    if not args.w:
+        WEBSITES = websitemanager.web_objects       
+    
+    if args.development:
+        DEVELOPMENT_ENV = True
+    
+    print("Enumerating through websites")
+    for index, key in enumerate(WEBSITES.keys()):
+        print(f"Index:{index}, Key:{key}")
+        print(WEBSITES[key])
+        response = web_check(target_object,WEBSITES[key])
+
+parser.add_argument('-u',action='append',help='Username to find accounts under (required if email not specified)')
+parser.add_argument('-e',action='append',help='Email to find accounts under (required if username not specified)')
+parser.add_argument('-w',help="The website(s) to check. comma-seperated (default: all)")
+parser.add_argument('--development',action='store_true',help="Provides additional debugging information during runtime (default: true)")
 if len(sys.argv)==1:
     parser.print_help(sys.stderr)
     sys.exit(1)
 
 args = parser.parse_args()
+print(args)
+if args.e and args.u:
+    parser.error("Please specify EITHER an email OR username")
 
-if args.websitelist:
-    #Will eventually list supported websites
-    pass
-    sys.exit(1)
+if args.e and len(args.e) >1 or args.u and len(args.u) >1:
+    parser.error("Please only provide one email or username for now.")
 
-if args.email and args.username:
-    parser.error("Please specify EITHER email or username")
+if not args.e and not args.u:
+    parser.error('Please specify an email or username using -e or -u ')
 
-if not args.email and not args.username:
-    parser.error('Pleasore specify an email or username using -e ')
 
-def main():
-    pass
 
+if __name__ == "__main__":
     main()
