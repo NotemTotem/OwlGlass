95c2834be4b0373738efa4aee1c08f260014ba0c
Nyonlexi <68531919+Nyonlexi@users.noreply.github.com>
2025-09-05 11:16:33 +1000

    Update dependencies, enhance form handling, and add CSRF protection
    - Added WTForms to project dependencies in pyproject.toml
    - Updated index.html to include hidden CSRF token and modified submit button for htmx integration
    - Added htmx script to base.html for dynamic content loading
    - Implemented CSRF protection in webgui.py
    - Recreated style.css with initial styles

--- src/Webgui/webgui.py
+++ src/Webgui/webgui.py
@@ -2,6 +2,11 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+#Global csrf protection
+from flask_wtf.csrf import CSRFProtect
+csrf = CSRFProtect(app)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('root/404.html'),404
@@ -9,6 +14,9 @@ def page_not_found(e):
 @app.route('/',methods=["GET","POST"])
 def index():
     return render_template('index.html')
+
+
+@app.route('/toolscripts/accountfinder',methods=["POST"])
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
