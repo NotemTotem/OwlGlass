d119129478277c0ec6e5875116122d233a0196a5
Nyonlexi <68531919+Nyonlexi@users.noreply.github.com>
2025-09-05 10:47:39 +1000

    Hotfix for pyproject.toml

--- src/Interfacer/interfacer.py
+++ src/Interfacer/interfacer.py
@@ -4,5 +4,8 @@ command = '''.venv\\Scripts\\python.exe "Toolscripts\\accountfinder.py" -e "jayd
 e = os.popen(command)
 output = e.readlines()
 
+def find_accounts(target,type):
+    if type and type =="email":
+        command = '''.venv\\Scripts\\python.exe "Toolscripts\\accountfinder.py" -e "{target}" --json --development'''
 for line in output: 
     print(line,end='')
