4de69d220021297d33d93494eac3b669045526c3
Nyonlexi <68531919+Nyonlexi@users.noreply.github.com>
2025-10-27 14:01:32 +1100

    Created an all-in-one script (init.bat) to setup and run this application.

---
+++ init.bat
@@ -0,0 +1,31 @@
+@echo off
+echo -
+echo -
+powershell -Command "Write-Host 'Starting setup, please be patient.' -ForegroundColor Green"
+
+if not exist .venv (
+echo -
+echo -
+powershell -Command "Write-Host 'Creating virtual environment.' -ForegroundColor Green"
+echo -
+echo -
+python -m venv .venv
+)
+if exist pyproject.toml (
+echo -
+echo -
+powershell -Command "Write-Host 'Requirements file found. Compiling and syncing packages.' -ForegroundColor Green"
+echo -
+echo -
+.venv\Scripts\pip.exe install .  
+) else (
+echo -
+echo -
+powershell -Command "Write-Host 'ERROR No pyproject file found.' -ForegroundColor Red"
+echo -
+echo -
+)
+powershell -Command "Write-Host 'Setup complete.' -ForegroundColor Green"
+echo -
+echo -
+cmd /k ".venv\Scripts\python.exe src\Webgui\webgui.py" 
