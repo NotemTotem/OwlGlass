c41f4135cf1e4029c0e3560fffbbcc47214ffc86
Nyonlexi <68531919+Nyonlexi@users.noreply.github.com>
2025-10-27 17:42:03 +1100

    Refactored main loop added websitecheck argument formatting improvements  and cleanup

--- Toolscripts/accountfinder.py
+++ Toolscripts/accountfinder.py
@@ -6,6 +6,7 @@ import json
 
 DEVELOPMENT = '--development' in sys.argv and not '--json' in sys.argv
 JSON_ONLY = '--json' in sys.argv
+WEBSITECHECK = '--websitecheck' in sys.argv
 
 class MyParser(argparse.ArgumentParser):
     def error(self,message):
@@ -26,23 +27,22 @@ def web_check(target_object,web_object):
     elif target_object["targetType"] == 'username':
         target = target_object["target"]
     else:
-        parser.error("targetType of {s!r}" % f'{target_object["targetType"]} not present in web_check.')
+        parser.debug_print(f"targetType of {target_object['targetType']!r} not present in web_check.")
 
-    url = web_object['url'].replace('<target>',target)
+    url = web_object['url'].replace('{}',target)
 
     if web_object['errorType'] == 'status_code':
         try:
-            r = requests.get('https://'+url)
+            r = requests.get(url)
         except Exception as e:
-            parser.error(e)
+            parser.debug_print(e)
+            return False,url
         if r.status_code == 200:
             return True,url
         else:
             return False,url
     else:
-        parser.error("status_code of {s!r}" % f'{web_object["status_code"]} not currently implemented.')
-def main():
-
+        parser.debug_print(f"status_code of {web_object['status_code']!r} not currently implemented.")
 
 def return_target_entry(target_string):
     #Check if target string is an email
@@ -63,8 +63,6 @@ def return_target_entry(target_string):
             'domain':None,
             'targetType':'username'
         }
-    if not args.w:
-        WEBSITES = websitemanager.web_objects       
 
 def main():
  
@@ -83,34 +81,67 @@ def main():
         'targets':[],
         'checks':[]
     }
-    for index, key in enumerate(WEBSITES.keys()):
-        parser.debug_print(f"Index:{index}, Key:{key}")
-        parser.debug_print(WEBSITES[key])
-        response,attempted_url = web_check(target_object,WEBSITES[key])
+
+    if not WEBSITECHECK:
+        target_string = args.t[0]
+        target_object = return_target_entry(target_string)
+        report['targets'].append(target_object)
+    #Tracking iteration count so we can set a max num of websites to check.
+    index = -1
+    for key, website in website_objects.items():
+        index+=1
+        parser.debug_print(f"Index:{index}, Website:{website}")
+        #This lets us set a max number of websites to check before printing the results.
+        if index > 4:
+            break
+        if index == 0:
+            #The first entry is just a schema object so we skip this.
+            continue
+        if 'isNSFW' in website:
+            #Some websites are NSFW as I got the json dataset from an opensource project. As such we must skip these.  
+            continue
+        if WEBSITECHECK:
+            target_string = website['username_claimed']
+            target_object = return_target_entry(target_string)
+
+        try:
+            response,attempted_url = web_check(target_object,website)
+        except Exception as e:
+            parser.warning(e)
+            response = False
+            attempted_url = website['url'].replace('{}',target_string)
 
         report_entry = {
-            'website':WEBSITES[key],
+            'website':website,
             'accountFound':response,
             'attemptedUrl':attempted_url
         }
         
         report['checks'].append(report_entry)
-    
+
     if JSON_ONLY:
         pass
     else:
-        print('\n'+'-'*30)
-        print(f'\nFINAL REPORT\n')
-        print('-'*30)
-        print(f"\n - USER INFO - ")
-        print(f"Target string: '{report['target']['target']}'\nTarget type: {report['target']['targetType']}")
-        print(f"\n - WEBSITES CHECKED - ")
+        length = 100
+        print('\n'+'-'*length)
+        print(f'\n{" "*((length//2)-(len('FINAL REPORT')//2))}FINAL REPORT\n')
+        print('-'*length)
+        if not WEBSITECHECK:
+            print(f"\n{' '*((length//2)-len('- USER INFO -')//2)}- USER INFO -")
+            print(f"Target: {report['targets'][0]['target']!r}")
+        print(f"\n{' '*((length//2)-len('- WEBSITES CHECKED -')//2)}- WEBSITES CHECKED -")
         for website in report['checks']:
-            print(f"Account found: {website['accountFound']}\nWebsite: https://{website['website']['urlMain']}\nAttempted url: https://{website['attemptedUrl']}")    
+            if website["accountFound"] == True:
+                print(f"Account found: {website['accountFound']}\nWebsite: https://{website['website']['urlMain']}\nAttempted url: https://{website['attemptedUrl']}")    
+        for website in report['checks']:
+            if website["accountFound"] == False:
+                print(f"Account found: {website['accountFound']}\nWebsite: https://{website['website']['urlMain']}\nAttempted url: https://{website['attemptedUrl']}")    
 
-parser.add_argument('-w',help="The website(s) to check. comma-seperated (default: all)")
+parser.add_argument('-t',action='append',help='Target string to find accounts under (email or username)')
+#parser.add_argument('-w',help="The website(s) to check. comma-seperated (default: all)")
 parser.add_argument('--development',action='store_true',help="Provides additional debugging information during runtime (default: true)")
 parser.add_argument('--json',action='store_true',help="Provides additional debugging information during runtime (default: true)")
+parser.add_argument('--websitecheck',action='store_true',help='Provides a list of websites that were incorrectly identified by accountfinder as not having a known user.')
 
 if len(sys.argv)==1:
     parser.print_help(sys.stderr)
