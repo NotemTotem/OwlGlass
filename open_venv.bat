e6960a971d828d289148d77304217e99f193fb6e
NotemTotem <notemlord@gmail.com>
2025-11-02 22:29:50 +1100

    open_venv works now

--- open_venv.bat
+++ open_venv.bat
@@ -1,8 +1,11 @@
 if exist .venv\Scripts\ (
   set "venv_path=%CD%\.venv\Scripts\activate" 
+  cmd /k "%venv_path%"
 ) else (
 	
   set "venv_path=%CD%\.venv\bin\Activate.ps1" 
+  powershell -NoExit -ExecutionPolicy Bypass -file %venv_path%
 )
 
-powershell -file %venv_path%
+
+ 
