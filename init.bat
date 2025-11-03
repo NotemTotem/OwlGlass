2501ca7d089acd0bf5ef22e18d012e5b5f95fb1f
NotemTotem <notemlord@gmail.com>
2025-11-03 12:08:20 +1100

    ?????????

--- init.bat
+++ init.bat
@@ -23,11 +23,7 @@ echo -
 powershell -Command "Write-Host 'Requirements file found. Compiling and syncing packages.' -ForegroundColor Green"
 echo -
 echo -
-<<<<<<< HEAD
-.venv\Scripts\pip.exe install .
-=======
 %venv_path%\pip.exe install .  
->>>>>>> e6960a971d828d289148d77304217e99f193fb6e
 ) else (
 echo -
 echo -
@@ -38,8 +34,5 @@ echo -
 powershell -Command "Write-Host 'Setup complete.' -ForegroundColor Green"
 echo -
 echo -
-<<<<<<< HEAD
-cmd /k ".venv\Scripts\python.exe src\Webgui\webgui.py"
-=======
+set "venv_path=%CD%"
 cmd /k "%venv_path%\python.exe src\Webgui\webgui.py" 
->>>>>>> e6960a971d828d289148d77304217e99f193fb6e
